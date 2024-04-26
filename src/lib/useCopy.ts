5719ce4365c3ae71782e42c94a762fea52a51abf
chongqiangchen <chongqiangchen@gmail.com>
2024-04-26 18:38:14 +0800

    fix: fix copy

--- src/lib/useCopy.ts
+++ src/lib/useCopy.ts
@@ -1,12 +1,44 @@
 import {useState} from "react";
 
+const unsecuredCopyToClipboard = (text: string) => {
+    const textArea = document.createElement("textarea");
+    textArea.value=text;
+    document.body.appendChild(textArea);
+    textArea.focus();textArea.select();
+    try{
+        document.execCommand('copy')
+    } catch(err) {
+        console.error('Unable to copy to clipboard',err)
+    }
+    document.body.removeChild(textArea)
+};
+
+/**
+ * Copies the text passed as param to the system clipboard
+ * Check if using HTTPS and navigator.clipboard is available
+ * Then uses standard clipboard API, otherwise uses fallback
+ */
+const copyToClipboard = (content: string) => {
+    return new Promise((resolve, reject) => {
+        if (navigator.clipboard && window.isSecureContext) {
+            navigator.clipboard.writeText(content).then(() => {
+                resolve(void 0);
+            }).catch(err => {
+                reject(err);
+            });
+        } else {
+            unsecuredCopyToClipboard(content);
+            resolve(void 0);
+        }
+    })
+};
 
 const useCopy = () => {
     const [isCopy, setIsCopy] = useState(false)
 
     const copy = (text: string) => {
         setIsCopy(true)
-        navigator.clipboard.writeText(text).then(() => {
+        copyToClipboard(text).then(() => {
             setTimeout(() => {
                 setIsCopy(false)
             }, 1000)
