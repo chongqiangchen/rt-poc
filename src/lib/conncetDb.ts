c4eb09a7b8c99b8b399cddb813971b990a7a6a5a
Bin Wang <bin.wang@thoughtworks.com>
2024-04-28 14:40:31 +0800

    update data and db connection

--- src/lib/conncetDb.ts
+++ src/lib/conncetDb.ts
@@ -9,9 +9,6 @@ const connectDb = async () => {
   try {
     await mongoose.connect(
       process.env.MONGO_URI || "mongodb://127.0.0.1:27017",
-      {
-        dbName: "local",
-      }
     );
     logger.info("Connected to MongoDB");
   } catch (error) {
